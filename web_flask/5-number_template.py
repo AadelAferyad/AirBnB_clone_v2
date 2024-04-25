538a47a6ecf5de653a6400e555ab3919664b410e
AadelAferyad <animevineu@gmail.com>
2024-04-25 18:39:25 +0100

    new route

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -43,7 +43,7 @@ def number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def nb_template(n):
     """ render page jinja2"""
-    return (render_template("5-number.html", n=n))
+    return (render_template('5-number.html', n=n))
 
 
 if __name__ == "__main__":
