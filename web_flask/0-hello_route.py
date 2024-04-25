89fd7bff85474736dc88be9a50b46364732bbb8b
AadelAferyad <animevineu@gmail.com>
2024-04-25 16:18:46 +0100

    web app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+starts a flask web application
+"""
+
+from flask import Flask
+
+app = Flask("__name__")
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """hello hbnb home route"""
+    return "Hello HBNB!"
